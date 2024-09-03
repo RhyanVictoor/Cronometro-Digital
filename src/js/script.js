ec201ef96a5d1ab1fa340571e0549f12406d4d2c
RhyanVictor <rhyansinho.04@gmail.com>
2024-09-03 18:38:12 -0400

    correção java script

--- src/js/script.js
+++ src/js/script.js
@@ -5,8 +5,8 @@ let timer = 0;
 let marks = [];
 
 const formatTime = (time) => {
-const hours = Math.floor(time / 360000);    
-const minutes = Math.floor((time % 360000) / 6000);
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor((time % 360000) / 6000);
     const seconds = Math.floor((time % 6000) / 100);
     const hundredths = time % 100;
 
